be96eb3a3f8cea593d62518dbc2b7bb72d40d20c
Cj-bc <cj-bc@y-modify.org>
2019-05-27 14:24:53 +0900

    Change: ctf4b/misk/sliding-puzzle/8puzzle> writing A* resolver [WIP]

--- 2019-seccon-beginners/misk/sliding-puzzle/8puzzle.hs
+++ 2019-seccon-beginners/misk/sliding-puzzle/8puzzle.hs
@@ -18,6 +18,11 @@ heuristic :: Node -> Cells -> Int
 heuristic (Node cells _ _ _) goal = sum $ zipWith (manhattan) (sort cells) (sort goal)
 
 
+a_star :: Node -> Cells -> Node
+a_star EmptyNode _ = EmptyNode
+a_star n@(Node c_cell status cost parent) goal | c_cell == goal = n
+                                               | otherwise      = 
+
 main = do
         let startCells = Cell.set [0,2,3,6,7,1,8,4,5]
             goalCells  = Cell.set [0,1,2,3,4,5,6,7,8]
